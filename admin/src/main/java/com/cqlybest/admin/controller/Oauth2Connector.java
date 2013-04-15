f03c00466ad2d00307b4ca24df2a69f2e2b9fa2f
belerweb <belerweb@gmail.com>
2013-04-15 21:25:03 +0800

    add project framework

---
+++ admin/src/main/java/com/cqlybest/admin/controller/Oauth2Connector.java
@@ -0,0 +1,23 @@
+package com.cqlybest.admin.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cqlybest.admin.oauth.QQAuthenticationToken;
+
+@Controller
+public class Oauth2Connector {
+
+  @RequestMapping("/connector/qq.do")
+  @ResponseBody
+  public void auth(@RequestParam String openId, @RequestParam String accessToken) {
+    // TODO validataion
+    SecurityContextHolder.getContext().setAuthentication(
+        new QQAuthenticationToken(openId, accessToken));
+    System.out.println("openId:" + openId);
+    System.out.println("accessToken:" + accessToken);
+  }
+}
