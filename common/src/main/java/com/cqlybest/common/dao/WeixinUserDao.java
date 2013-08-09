8c439f04b3faa7d2d2e4b1e61f070f51cd0555dc
belerweb <belerweb@gmail.com>
2013-08-09 11:49:39 +0800

    Fix WeixinUserDao autowire issue

--- common/src/main/java/com/cqlybest/common/dao/WeixinUserDao.java
+++ common/src/main/java/com/cqlybest/common/dao/WeixinUserDao.java
@@ -1,7 +1,10 @@
 package com.cqlybest.common.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.belerweb.weixin.mp.WeixinUser;
 
+@Repository
 public class WeixinUserDao extends AbstractDao<WeixinUser, String> {
 
   protected WeixinUserDao() {
