76fcc16d0f69646e12931dfe3b82e9e526ce4648
belerweb <belerweb@gmail.com>
2013-09-26 22:53:46 +0800

    fix issue

--- common/src/main/java/com/cqlybest/common/mongo/service/ImageService.java
+++ common/src/main/java/com/cqlybest/common/mongo/service/ImageService.java
@@ -1,38 +1,16 @@
 package com.cqlybest.common.mongo.service;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.cqlybest.common.mongo.bean.Image;
 import com.cqlybest.common.mongo.dao.MongoDb;
-
-import eu.medsea.util.MimeUtil;
 
 @Service("mongoImageService")
 public class ImageService {
 
   @Autowired
   private MongoDb mongoDb;
-
-  public Image upload(MultipartFile file) throws IOException {
-    String name = file.getOriginalFilename();
-    Image image = new Image();
-    image.setId(UUID.randomUUID().toString());
-    image.setExtension(MimeUtil.getFileExtension(name).toLowerCase());
-    image.setContentType(MimeUtil.getMimeType(name).toLowerCase().split(",")[0]);
-    image.setData(file.getBytes());
-
-    Date now = new Date();
-    image.setCreatedTime(now);
-    image.setLastUpdated(now);
-
-    return mongoDb.getMongoDao().createObject("Image", image);
-  }
 
   public void addImage(Image image) {
     mongoDb.createObject("Image", image);
