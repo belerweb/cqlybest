a945c20ae095de4c8f7c4014956d6932a43d42cf
belerweb <belerweb@gmail.com>
2013-10-07 18:19:38 +0800

    add Example beans

---
+++ common/src/main/java/com/cqlybest/common/bean/example/Example.java
@@ -0,0 +1,45 @@
+package com.cqlybest.common.bean.example;
+
+/**
+ * 标杆
+ */
+public class Example {
+
+  private String id;
+  private String name; // 标杆名称
+  private String logo; // 标杆
+  private String description;// 标杆介绍
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLogo() {
+    return logo;
+  }
+
+  public void setLogo(String logo) {
+    this.logo = logo;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+}
